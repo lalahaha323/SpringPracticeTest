e808d91e260a01f4f0501496e04b8de58267cdf5
lalahaha323 <2665687186@qq.com>
2019-03-29 21:53:10 +0800

    构造注入

--- src/main/java/com/imooc/ioc/injection/service/InjectionServiceImpl.java
+++ src/main/java/com/imooc/ioc/injection/service/InjectionServiceImpl.java
@@ -6,6 +6,11 @@ public class InjectionServiceImpl implements InjectionService {
 
     private InjectionDAO injectionDAO;
 
+    //构造器注入
+    public InjectionServiceImpl(InjectionDAO injectionDAO) {
+        this.injectionDAO = injectionDAO;
+    }
+
     //设值注入
     public void setInjectionDAO(InjectionDAO injectionDAO) {
         this.injectionDAO = injectionDAO;
