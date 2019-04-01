e98df606a0a0beda280922558eb7af6b154baac1
lalahaha323 <2665687186@qq.com>
2019-04-01 13:04:42 +0800

    @Configuration启动容器+@Bean注册Bean，@Bean下管理bean的生命周期

--- src/main/java/com/demo/configuration/TestConfiguration.java
+++ src/main/java/com/demo/configuration/TestConfiguration.java
@@ -1,10 +1,18 @@
 package com.demo.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class TestConfiguration {
     public TestConfiguration() {
         System.out.println("testConfiguration容器启动初始化");
+    }
+    //@Bean注解注册bean，同时可以指定初始化和销毁方法
+    @Bean(name="testBean", initMethod="start", destroyMethod="cleanUp")
+    @Scope("prototype")
+    public TestBean testBean() {
+        return new TestBean();
     }
 }
--- src/main/java/com/demo/configuration/TestMain.java
+++ src/main/java/com/demo/configuration/TestMain.java
@@ -6,5 +6,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class TestMain {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(TestConfiguration.class);
+        //如果是加载spring-context.xml文件：
+        //ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        TestBean tb = (TestBean)context.getBean("testBean");
+        tb.sayHello();
     }
 }
