85921e440495833a49d772d51dfe07834f93b5ea
lalahaha323 <2665687186@qq.com>
2019-04-07 10:37:10 +0800

    增加一个诗人来歌颂骑士

--- src/main/java/sia/knights/BraveKnight.java
+++ src/main/java/sia/knights/BraveKnight.java
@@ -3,12 +3,16 @@ package sia.knights;
 public class BraveKnight implements Knight{
 
     private Quest quest;
+    private Minstrel minstrel;
 
-    public BraveKnight(Quest quest) {
+    public BraveKnight(Quest quest, Minstrel minstrel) {
         this.quest =  quest;
+        this.minstrel = minstrel;
     }
 
     public void embarkOnQuest() {
+        minstrel.singBeforeQuest();
         quest.embark();
+        minstrel.singAfterQuest();
     }
 }
