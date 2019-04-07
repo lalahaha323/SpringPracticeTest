49296ea25150b37a615e9718619c0f901ea31154
lalahaha323 <2665687186@qq.com>
2019-04-07 10:18:42 +0800

    通过mock来测试BraveKnight

--- src/main/java/sia/knights/BraveKnight.java
+++ src/main/java/sia/knights/BraveKnight.java
@@ -5,7 +5,7 @@ public class BraveKnight implements Knight{
     private Quest quest;
 
     public BraveKnight(Quest quest) {
-        this.quest = quest;
+        this.quest =  quest;
     }
 
     public void embarkOnQuest() {
