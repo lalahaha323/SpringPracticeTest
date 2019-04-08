204f0cef0b8c28a54269b3b453b6d37d68344653
lalahaha323 <2665687186@qq.com>
2019-04-08 20:05:01 +0800

    装配集合

--- src/test/java/chapter_02/autoconfig/soundsystem/CDPlayerTest.java
+++ src/test/java/chapter_02/autoconfig/soundsystem/CDPlayerTest.java
@@ -2,12 +2,9 @@ package chapter_02.autoconfig.soundsystem;
 
 import static org.junit.Assert.*;
 
-import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,23 +12,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(classes=CDPlayerConfig.class)
 public class CDPlayerTest {
 
-    @Rule
-    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
-
-    @Autowired
-    private MediaPlayer player;
-
     @Autowired
     private CompactDisc cd;
 
     @Test
     public void cdShouldNotBeNull() {
         assertNotNull(cd);
-    }
-
-    @Test
-    public void play() {
-        player.play();
-        assertEquals("Playing Sgt. Pepper's Lonely Hearts Club Band byThe Beatles\n", log.getLog());
     }
 }
