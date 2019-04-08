cd4135c6a64285d4c9bcb3812eb1cc6ade8acacb
lalahaha323 <2665687186@qq.com>
2019-04-08 21:30:44 +0800

    在JavaConfig中引用XML配置

---
+++ src/main/java/chapter_02/mixedxonfig/soundsystem/CompactDisc.java
@@ -0,0 +1,5 @@
+package chapter_02.mixedxonfig.soundsystem;
+
+public interface CompactDisc {
+    void play();
+}
---
+++ src/test/java/chapter_02/mixedconfig/soundsystem/ImportJavaConfigTest.java
@@ -0,0 +1,29 @@
+package chapter_02.mixedconfig.soundsystem;
+
+import static org.junit.Assert.assertEquals;
+
+import chapter_02.mixedxonfig.soundsystem.MediaPlayer;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:cdplayer-config.xml")
+public class ImportJavaConfigTest {
+
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+    @Autowired
+    private MediaPlayer player;
+
+    @Test
+    public void play() {
+        player.play();
+    }
+
+}
