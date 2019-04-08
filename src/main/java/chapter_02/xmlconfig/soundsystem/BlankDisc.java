99d2fc7cea4d93ab8d7c20caafec8218bd0d51ec
lalahaha323 <2665687186@qq.com>
2019-04-08 20:07:41 +0800

    使用set

--- src/main/java/chapter_02/xmlconfig/soundsystem/BlankDisc.java
+++ src/main/java/chapter_02/xmlconfig/soundsystem/BlankDisc.java
@@ -1,13 +1,14 @@
 package chapter_02.xmlconfig.soundsystem;
 import java.util.List;
+import java.util.Set;
 
 public class BlankDisc implements CompactDisc{
 
     private String title;
     private String artist;
-    private List<String> tracks;
+    private Set<String> tracks;
 
-    public BlankDisc(String title, String artist, List<String> tracks) {
+    public BlankDisc(String title, String artist, Set<String> tracks) {
         this.title = title;
         this.artist = artist;
         this.tracks = tracks;
